dc7fbac6798477d41a2001f026d7e95db2db5c59
Jan-BaptistVandeuren <jb.alexis@outlook.fr>
2024-03-01 17:08:22 +0100

    modified a working version of the pipeline yaml generator from the excel with a template excel build

--- GenerateYaml.py
+++ GenerateYaml.py
@@ -1,22 +1,30 @@
 import pandas as pd
 import yaml
+from collections import OrderedDict
+
+# A custom representer for OrderedDict that treats it as a regular dict
+def dict_representer(dumper, data):
+    return dumper.represent_dict(data.items())
+
+# Add the custom representer to the Dumper
+yaml.add_representer(OrderedDict, dict_representer)
 
 # Read the Excel file
 df = pd.read_excel('pipelines.xlsx')
 
 # Prepare the YAML content
-content = {
-    'variables': {
+content = OrderedDict([
+    ('variables', {
         'script_build': 'Scripts\\build.ps1',
-    },
-    'resources': {
+    }),
+    ('resources', {
         'pipelines': [],
-    },
-    'pool': {
+    }),
+    ('pool', {
         'vmImage': 'windows-latest',
-    },
-    'steps': [],
-}
+    }),
+    ('steps', []),
+])
 
 # Add each pipeline to the YAML content
 for i, row in df.iterrows():
@@ -24,20 +32,20 @@ for i, row in df.iterrows():
     content['variables'][f'{variable_prefix}'] = row['pipeline']
     content['variables'][f'{variable_prefix}Source'] = row['pipelineSource']
     content['variables'][f'{variable_prefix}JsonFile'] = row['jsonFile']
-    content['resources']['pipelines'].append({
-        'pipeline': f'${{ variables.{variable_prefix} }}',
-        'source': f'${{ variables.{variable_prefix}Source }}',
-        'trigger': True,
-    })
-    content['steps'].append({
-        'task': f'${{ variables.{variable_prefix} }}',
-        'inputs': {
-            'targetType': 'filePath',
-            'filePath': '${{ variables.script_build }}',
-            'arguments': f'-JsonFilePath "${{ variables.{variable_prefix}JsonFile }}"',
-        },
-        'condition': f'eq(resources.pipeline.${{variables.{variable_prefix}}}.result, \'Succeeded\')',
-    })
+    content['resources']['pipelines'].append(OrderedDict([
+        ('pipeline', f'${{ variables.{variable_prefix} }}'),
+        ('source', f'${{ variables.{variable_prefix}Source }}'),
+        ('trigger', True),
+    ]))
+    content['steps'].append(OrderedDict([
+        ('task', f'${{ variables.{variable_prefix} }}'),
+        ('inputs', OrderedDict([
+            ('targetType', 'filePath'),
+            ('filePath', '${{ variables.script_build }}'),
+            ('arguments', f'-JsonFilePath "${{ variables.{variable_prefix}JsonFile }}"'),
+        ])),
+        ('condition', f'eq(resources.pipeline.${{variables.{variable_prefix}}}.result, \'Succeeded\')'),
+    ]))
 
 # Write the YAML file
 with open('pipelines.yaml', 'w') as f:
